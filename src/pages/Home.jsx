184152498609fb2498a51f967bfe63d6d60b7581
Muhammad Arifien Syachrizal <arifiensr@gmail.com>
2023-04-19 13:36:38 +0700

    Create framework

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Navbar from '../components/Navbar'
+import BootstrapThemeKit from './BootstrapThemeKit'
+
+export default function Home() {
+  return (
+    <>
+      <BootstrapThemeKit/>
+    </>
+  )
+}
